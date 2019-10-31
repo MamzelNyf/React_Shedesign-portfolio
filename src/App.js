10768bedd229aba504c404806aaee806b4e73f0b
MamzelNyf <fanny.lefert@hotmail.fr>
2019-10-31 11:21:46 +0800

    readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    titel: 'Fanny Lefert',
+    headerLinks: [
+      {title: 'Home', path: '/' },
+      {title: 'About', path: '/about' },
+      {title: 'Contact', path: '/contact' },
+    ],
+    home: {
+      title: 'Creating website is fun. That\'s a fact.',
+      subTitle: 'Where Design & Code meet',
+      text: 'Check out my projects below'
+    },
+    about: {
+      title: 'About',
+    },
+    contact: {
+      title: 'Let\'s talk',
+    }
+  }
+}
+
+  render(){
+    return (
+      <div>Hello from React</div>
+    );
+  }
 }
 
 export default App;
